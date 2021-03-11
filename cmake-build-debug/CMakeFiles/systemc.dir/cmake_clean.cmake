33d3a197cb0b4a2ee346e82d879d90266ae917a6
shashank-shark <shashankreddy231@gmail.com>
2021-03-12 00:37:10 +0530

    initial commit

---
+++ cmake-build-debug/CMakeFiles/systemc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/systemc.dir/main.c.o"
+  "libsystemc.dll.a"
+  "systemc.exe"
+  "systemc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/systemc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/systemc.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for systemc.
+# This may be replaced when dependencies are built.
